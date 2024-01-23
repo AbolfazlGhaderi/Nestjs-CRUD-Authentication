bb79680c26a1ffb235da01265d1f81f73b15da96
Abolfazl Ghaderi <brianprogrammer0@gmail.com>
2024-01-23 08:56:38 +0330

    Add -> Create Resource User using Nestjs Add -> Connect to the DB (DB Name : nestjs-crud )

---
+++ src/users/users.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDto } from './dto/user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() userDto: UserDto) {
+    return this.usersService.create(userDto);
+  }
+
+  // @Get()
+  // findAll() {
+  //   return this.usersService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.usersService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(+id, updateUserDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.usersService.remove(+id);
+  // }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
+
+@Injectable()
+export class UsersService {
+  create(userData: UserDto) {
+    return 'This action adds a new user';
+  }
+
+  // findAll() {
+  //   return `This action returns all users`;
+  // }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} user`;
+  // }
+
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
+}
